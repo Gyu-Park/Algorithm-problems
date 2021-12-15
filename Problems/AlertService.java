c3ccdecf25d60e2dd7ad2b2420db6e9f64327044
Gyu-Park <kyesung8282@gmail.com>
2021-12-15 10:00:40 -0500

    Problem 5: Alert Service

---
+++ Problems/AlertService.java
@@ -0,0 +1,46 @@
+package Problems;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+interface AlertDAO {
+
+    UUID addAlert(Date time);
+
+    Date getAlert(UUID id);
+
+}
+
+public class AlertService {
+    private final AlertDAO storage;
+
+    public AlertService(AlertDAO storage) {
+        this.storage = storage;
+    }
+
+    public UUID raiseAlert() {
+        return this.storage.addAlert(new Date());
+    }
+
+    public Date getAlertTime(UUID id) {
+        return this.storage.getAlert(id);
+    }
+}
+
+class MapAlertDAO implements AlertDAO {
+    private final Map<UUID, Date> alerts = new HashMap<UUID, Date>();
+
+    @Override
+    public UUID addAlert(Date time) {
+        UUID id = UUID.randomUUID();
+        this.alerts.put(id, time);
+        return id;
+    }
+
+    @Override
+    public Date getAlert(UUID id) {
+        return this.alerts.get(id);
+    }
+}
