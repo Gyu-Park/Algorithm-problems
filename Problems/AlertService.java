e100852a0e2c52ca77bbe8f2012fd7583ce05dfa
Gyu-Park <kyesung8282@gmail.com>
2021-12-15 10:01:23 -0500

    Problem 5: Alert Service

--- Problems/AlertService.java
+++ Problems/AlertService.java
@@ -1,3 +1,11 @@
+/**
+Refactor the AlertService and MapAlertDAO classes:
+
+Create a new package-private interface, named AlertDAO, that contains the same methods as MapAlertDAO.
+MapAlertDAO should implement the AlertDAO interface.
+AlertService should have a constructor that accepts AlertDAO.
+The raiseAlert and getAlertTime methods should use the object passed through the constructor.
+ **/
 package Problems;
 
 import java.util.Date;
