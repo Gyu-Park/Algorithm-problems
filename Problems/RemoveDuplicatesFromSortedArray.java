f2acb5e226bbac6aa70969a5e1671998e7e18b6b
Gyu-Park <kyesung8282@gmail.com>
2021-12-20 15:16:43 -0500

    Problem 18: Remove Duplicates from Sorted Array

---
+++ Problems/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,40 @@
+/**
+ * Given an integer array nums sorted in non-decreasing order, 
+ * remove the duplicates in-place such that each unique element appears only once. 
+ * The relative order of the elements should be kept the same.
+ * 
+ * Since it is impossible to change the length of the array in some languages, 
+ * you must instead have the result be placed in the first part of the array nums. 
+ * More formally, if there are k elements after removing the duplicates, 
+ * then the first k elements of nums should hold the final result. 
+ * It does not matter what you leave beyond the first k elements.\
+ * 
+ * Return k after placing the final result in the first k slots of nums.
+ * 
+ * Do not allocate extra space for another array. 
+ * You must do this by modifying the input array in-place with O(1) extra memory.
+ */
+package Problems;
+
+import java.util.Arrays;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        nums = Arrays.copyOf(nums, count);
+
+        return nums.length;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicates(nums));
+    }
+}
