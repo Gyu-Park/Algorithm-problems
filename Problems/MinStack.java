9b999d2ed52264672629aa8f60ce24e500062a65
Gyu-Park <kyesung8282@gmail.com>
2021-12-26 14:19:58 -0500

    Problem 46: Min Stack

---
+++ Problems/MinStack.java
@@ -0,0 +1,62 @@
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * 
+ * Implement the MinStack class:
+ * 
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
+ */
+package Problems;
+
+public class MinStack {
+    private Node head;
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+        if (head == null)
+            head = new Node(val, val, null);
+        else
+            head = new Node(val, Math.min(val, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top()); // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+}
