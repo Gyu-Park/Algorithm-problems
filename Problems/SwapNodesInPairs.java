91b7d09d3572867848895fe176ffe83843cd6f1e
Gyu Park <kyesung8282@gmail.com>
2022-02-24 08:50:26 -0500

    Problem 141: Swap Nodes in Pairs

--- Problems/SwapNodesInPairs.java
+++ Problems/SwapNodesInPairs.java
@@ -8,17 +8,51 @@ public class SwapNodesInPairs {
     public static ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null)
             return head;
-        ListNode dummy = new ListNode(0);
-        ListNode prev = head.next;
-        ListNode curr = head;
-        head = head.next;
-        prev.next = head;
-        dummy.next = prev;
+        head = recurrsion(head.next.next, head.next, head);
+        return head;
+    }
 
-        while (head != null) {
-            
+    private static ListNode recurrsion(ListNode head, ListNode prev, ListNode curr) {
+        prev.next = curr;
+        if (head == null) {
+            curr.next = head;
+            return prev;
+        } else if (head.next != null) {
+            curr.next = head.next;
+        } else {
+            curr.next = head;
+            return prev;
         }
-        return dummy;
+        recurrsion(head.next.next, head.next, head);
+        return prev;
+    }
+
+    // shorter solution
+    public static ListNode shorterSwapPairs(ListNode head) {
+        if ((head == null)||(head.next == null))
+            return head;
+        ListNode node = head.next;
+        head.next = swapPairs(head.next.next);
+        node.next = head;
+        return node;
+    }
+
+    // iterative solution - better memory usage
+    public static ListNode iterSwapPairs(ListNode head) {
+        if (head == null || head.next == null) 
+            return head;
+        ListNode curr = head;
+        ListNode newHead = head.next;
+        while (curr != null && curr.next != null) {
+            ListNode tmp = curr;
+            curr = curr.next;
+            tmp.next = curr.next;
+            curr.next = tmp;
+            curr = tmp.next;
+            if (curr != null && curr.next != null) 
+                tmp.next = curr.next;
+        }
+        return newHead;
     }
 
     static class ListNode {
@@ -36,14 +70,28 @@ public class SwapNodesInPairs {
         ListNode node1 = new ListNode(2);
         ListNode node2 = new ListNode(3);
         ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(6);
+        ListNode node6 = new ListNode(7);
         head.next = node1;
         node1.next = node2;
         node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
 
+        /**
         ListNode result = swapPairs(head);
         while (result != null) {
             System.out.print(result.val + " ");
             result = result.next;
+        }
+        **/
+
+        ListNode result2 = iterSwapPairs(head);
+        while (result2 != null) {
+            System.out.print(result2.val + " ");
+            result2 = result2.next;
         }
     }
 }
