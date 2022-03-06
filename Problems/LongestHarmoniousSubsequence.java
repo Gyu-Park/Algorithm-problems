b6a06a5aee45237014be10b5d4a1886afc222d9c
Gyu-Park <kyesung8282@gmail.com>
2022-03-05 22:44:05 -0500

    Problem 150: Longest Harmonious Subsequence

---
+++ Problems/LongestHarmoniousSubsequence.java
@@ -0,0 +1,21 @@
+/**
+ * We define a harmonious array as an array where the difference between its maximum value and its minimum value is exactly 1.
+ * 
+ * Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
+ * 
+ * A subsequence of array is a sequence that can be derived from the array 
+ * by deleting some or no elements without changing the order of the remaining elements.
+ */
+
+package Problems;
+
+public class LongestHarmoniousSubsequence {
+    public static int findLHS(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 3, 2, 2, 5, 2, 3, 7 };
+        System.out.println(findLHS(nums));
+    }
+}
