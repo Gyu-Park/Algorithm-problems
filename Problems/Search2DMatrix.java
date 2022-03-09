e22a474ad62ab37ad03f4862e2b051c95d1d4197
Gyu Park <kyesung8282@gmail.com>
2022-03-09 18:32:54 -0500

    Problem 154: Search 2D Matrix

---
+++ Problems/Search2DMatrix.java
@@ -0,0 +1,13 @@
+package Problems;
+
+public class Search2DMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        
+    }
+    
+    public static void main(String[] args) {
+        int[][] matrix = { {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60} };
+        int target = 13;
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
