74b0d2e00ba33664cb8102aeb4cc7e18276140f0
Gyu Park <kyesung8282@gmail.com>
2022-03-10 10:23:45 -0500

    Problem 155: Minimum Path Sum

--- Problems/MinimumPathSum.java
+++ Problems/MinimumPathSum.java
@@ -45,9 +45,9 @@ public class MinimumPathSum {
                 if (i == 0 && j == 0) {
                     grid[i][j] = grid[i][j];
                 } else if (i != 0 && j == 0) {
-                    grid[i][j] = grid[i][j] + grid[i - 1][j];
+                    grid[i][j] += grid[i - 1][j];
                 } else if (i == 0 && j != 0) {
-                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                    grid[i][j] += grid[i][j - 1];
                 } else {
                     grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
                 }
