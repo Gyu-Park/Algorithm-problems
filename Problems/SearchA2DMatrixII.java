a973723a8cb357af88a9ce785284084b08bbf7c5
Gyu Park <kyesung8282@gmail.com>
2022-04-12 19:08:05 -0400

    Problem 184: Search a 2D Matrix II

--- Problems/SearchA2DMatrixII.java
+++ Problems/SearchA2DMatrixII.java
@@ -40,7 +40,25 @@ public class SearchA2DMatrixII {
         }
         return false;
     }
-    
+
+    // another solution searching from top right
+    public boolean anotherSearchMatrix(int[][] matrix, int target) {
+        if (matrix.length < 1 || matrix[0].length < 1)
+            return false;
+        int col = matrix[0].length - 1;
+        int row = 0;
+        while (col >= 0 && row <= matrix.length - 1) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                col--;
+            } else if (target > matrix[row][col]) {
+                row++;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[][] matrix = {
                             {1, 4, 7, 11, 15},
