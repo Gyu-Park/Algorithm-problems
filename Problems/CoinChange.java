6b200f22d8466d8fd4a7083f6e4b9045132677f1
Gyu-Park <kyesung8282@gmail.com>
2022-04-15 23:07:59 -0400

    Problem 188: Coin Change

---
+++ Problems/CoinChange.java
@@ -0,0 +1,22 @@
+/**
+ * You are given an integer array coins representing coins of different denominations 
+ * and an integer amount representing a total amount of money.
+ * 
+ * Return the fewest number of coins that you need to make up that amount. 
+ * If that amount of money cannot be made up by any combination of the coins, return -1.
+ * 
+ * You may assume that you have an infinite number of each kind of coin.
+ */
+package Problems;
+
+public class CoinChange {
+    public static int coinChange(int[] coins, int amount) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] coins = { 1, 2, 5 };
+        int amount = 11;
+        System.out.println(coinChange(coins, amount));
+    }
+}
