2705a58f5033d72b6e223292049062e25b4ddbd3
Gyu Park <kyesung8282@gmail.com>
2022-04-19 22:29:19 -0400

    Problem 188: Coin Change

--- Problems/CoinChange.java
+++ Problems/CoinChange.java
@@ -9,60 +9,16 @@
  */
 package Problems;
 
-import java.util.*;
-
 public class CoinChange {
     public static int coinChange(int[] coins, int amount) {
-        if (amount == 0)
-            return 0;
-        Arrays.sort(coins);
-        List<Integer> list = new ArrayList<>();
-        long sum = 0;
-        for (int i = coins.length - 1; i >= 0; i--) {
-            list.add(coins[i]);
-            sum += coins[i];
-            if (sum < amount) {
-                sum = helper(coins, amount, sum, list, i);
-            }
-            if (sum == amount) {
-                break;
-            }
-            sum -= coins[i];
-            list.remove(list.size() - 1);
-        }
-        return list.size() == 0 ? -1 : list.size();
-    }
-
-    private static long helper(int[] coins, int amount, long sum, List<Integer> list, int j) {
-        if (sum == amount)
-            return sum;
-        if (sum > amount) {
-            sum -= list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-            return sum;
-        }
-        for (int i = j; i >= 0; i--) {
-            list.add(coins[i]);
-            sum += coins[i];
-            sum = helper(coins, amount, sum, list, i);
-            if (sum == amount)
-                return sum;
-        }
-        sum -= list.get(list.size() - 1);
-        list.remove(list.size() - 1);
-        return sum;
-    }
-
-    public static int anotherCoinChange(int[] coins, int amount) {
         if(amount<1) return 0;
         return helper(coins, amount, new int[amount]);
     }
     
-    // rem: remaining coins after the last step; count[rem]: minimum number of coins to sum up to rem
     private static int helper(int[] coins, int rem, int[] count) {
-        if (rem < 0) return -1; // not valid
-        if (rem == 0) return 0; // completed
-        if (count[rem - 1] != 0) return count[rem - 1]; // already computed, so reuse
+        if (rem < 0) return -1;
+        if (rem == 0) return 0;
+        if (count[rem - 1] != 0) return count[rem - 1];
         int min = Integer.MAX_VALUE;
         for (int coin : coins) {
             int res = helper(coins, rem - coin, count);
@@ -76,6 +32,6 @@ public class CoinChange {
     public static void main(String[] args) {
         int[] coins = { 1, 2, 5 };
         int amount = 11;
-        System.out.println(anotherCoinChange(coins, amount));
+        System.out.println(coinChange(coins, amount));
     }
 }
