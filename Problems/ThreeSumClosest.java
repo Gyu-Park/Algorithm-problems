dc4a6a5ffdf142d4158e1cf364ea3d10328b9918
Gyu Park <kyesung8282@gmail.com>
2022-04-26 22:10:14 -0400

    Problem 199: 3Sum Closest

---
+++ Problems/ThreeSumClosest.java
@@ -0,0 +1,21 @@
+/**
+ * Given an integer array nums of length n and an integer target, 
+ * find three integers in nums such that the sum is closest to target.
+ * 
+ * Return the sum of the three integers.
+ * 
+ * You may assume that each input would have exactly one solution.
+ */
+package Problems;
+
+public class ThreeSumClosest {
+    public static int threeSumClosest(int[] nums, int target) {
+        
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = { -1, 2, 1, -4 };
+        int target = 1;
+        System.out.println(threeSumClosest(nums, target));
+    }
+}
