a630cb1e68473fbbd684d88adf17969b6965e099
Gyu Park <kyesung8282@gmail.com>
2022-04-29 16:43:10 -0400

    Problem 199: 3Sum Closest

--- Problems/ThreeSumClosest.java
+++ Problems/ThreeSumClosest.java
@@ -8,15 +8,43 @@
  */
 package Problems;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
-    // two points
+    // a solution with using two pointers
     public static int threeSumClosest(int[] nums, int target) {
-        
+        Arrays.sort(nums);
+        int pre = Integer.MAX_VALUE;
+        int res = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int secondPointer = i + 1;
+            int endPointer = nums.length - 1;
+            while (secondPointer != endPointer) {
+                int sum = nums[i] + nums[secondPointer] + nums[endPointer];
+                if (sum > target) {
+                    endPointer--;
+                    if (pre < sum - target) {
+                        continue;
+                    }
+                    pre = sum - target;
+                } else if (sum < target) {
+                    secondPointer++;
+                    if (pre < target - sum) {
+                        continue;
+                    }
+                    pre = target - sum;
+                } else if (sum == target) {
+                    return sum;
+                }
+                res = sum;
+            }
+        }
+        return res;
     }
     
     public static void main(String[] args) {
-        int[] nums = { -1, 2, 1, -4 };
-        int target = 1;
+        int[] nums = { 1, 2, 4, 8, 16, 32, 64, 128 };
+        int target = 82;
         System.out.println(threeSumClosest(nums, target));
     }
 }
