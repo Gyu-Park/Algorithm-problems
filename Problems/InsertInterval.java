cfca169bf0498ca2aed7113275446fba715debbe
Gyu-Park <kyesung8282@gmail.com>
2022-05-07 23:01:00 -0400

    Problem 206: Insert Interval

--- Problems/InsertInterval.java
+++ Problems/InsertInterval.java
@@ -12,12 +12,74 @@ package Problems;
 
 public class InsertInterval {
     public static int[][] insert(int[][] intervals, int[] newInterval) {
+        if (intervals.length == 0)
+            return new int[][] { { newInterval[0], newInterval[1] } };
 
+        int k = 2;
+        int i;
+        for (i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] > newInterval[0]) {
+                k = 0;
+                break;
+            } else if (intervals[i][1] > newInterval[0]) {
+                k = 1;
+                break;
+            }
+        }
+
+        int kk = 2;
+        int j;
+        for (j = i; j < intervals.length; j++) {
+            if (intervals[j][0] > newInterval[1]) {
+                kk = 0;
+                j--;
+                break;
+            } else if (intervals[j][1] > newInterval[1]) {
+                kk = 1;
+                break;
+            }
+        }
+
+        int newLen = i + (intervals.length - j);
+        int[][] res = new int[newLen][2];
+        int ii;
+        for (ii = 0; ii < res.length; ii++) {
+            if (ii == i) {
+                if (k == 0)
+                    res[ii][0] = newInterval[0];
+                else if (k == 1)
+                    res[ii][0] = intervals[ii][0];
+
+                if (kk == 0)
+                    res[ii][1] = newInterval[1];
+                else if (kk == 1)
+                    res[ii][1] = intervals[j][1];
+                else if (kk == 2) {
+                    res[ii][1] = newInterval[1];
+                    return res;
+                }
+                break;
+            }
+            res[ii][0] = intervals[ii][0];
+            res[ii][1] = intervals[ii][1];
+        }
+
+        for (int jj = j + 1; jj < intervals.length; jj++) {
+            res[++ii][0] = intervals[jj][0];
+            res[ii][1] = intervals[jj][1];
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
         int[][] intervals = { { 1, 2 }, { 3, 5 }, { 6, 7 }, { 8, 10 }, { 12, 16 } };
         int[] newInterval = { 4, 8 };
-        System.out.println(insert(intervals, newInterval));
+        int[][] intervals2 = { { 1, 3 }, { 6, 9 } };
+        int[] newInterval2 = { 2, 5 };
+        int[][] res = insert(intervals, newInterval);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(" [" + res[i][0] + ", " + res[i][1] + "] ");
+        }
     }
 }
