92e7160c081784fccede107b34c49cb659cf836f
Gyu-Park <kyesung8282@gmail.com>
2022-05-09 22:10:04 -0400

    Problem 207: Spiral Matrix II

---
+++ Problems/SpiralMatrixII.java
@@ -0,0 +1,15 @@
+/**
+ * Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+ */
+package Problems;
+
+public class SpiralMatrixII {
+    public static int[][] generateMatrix(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(generateMatrix(n));
+    }
+}
