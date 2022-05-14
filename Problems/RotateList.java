b7322064dc3bb3cc73631f94cbe8df076cb7d047
Gyu Park <kyesung8282@gmail.com>
2022-05-13 20:44:24 -0400

    Problem 208: Rotate List

---
+++ Problems/RotateList.java
@@ -0,0 +1,29 @@
+package Problems;
+
+public class RotateList {
+    public static ListNode rotateRight(ListNode head, int k) {
+        
+    }
+    
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        int k = 2;
+        System.out.println(rotateRight(head, k));
+    }
+}
