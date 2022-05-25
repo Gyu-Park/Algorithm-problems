8a14dec95700a2185e6718a9eb65ab51a47b2716
Gyu Park <kyesung8282@gmail.com>
2022-05-24 21:00:29 -0400

    Problem 217: Subsets II

--- Problems/SubsetsII.java
+++ Problems/SubsetsII.java
@@ -8,12 +8,31 @@ package Problems;
 import java.util.*;
 
 public class SubsetsII {
+    // recursive solution
+    // time complexity O(2^n)
+    // space complexity O(2^n)
     public static List<List<Integer>> subsetsWithDup(int[] nums) {
-        
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        Arrays.sort(nums);
+        return helper(nums, 0, res, new ArrayList<>());
+    }
+
+    private static List<List<Integer>> helper(int[] nums, int index, List<List<Integer>> res, List<Integer> list) {
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i - 1] == nums[i])
+                continue;
+            list.add(nums[i]);
+            res.add(new ArrayList<>(list));
+            res = helper(nums, i + 1, res, list);
+            list.remove(list.size() - 1);
+        }
+
+        return res;
     }
     
     public static void main(String[] args) {
-        int[] nums = {};
+        int[] nums = {1, 2, 2};
         List<List<Integer>> res = subsetsWithDup(nums);
         for (List<Integer> list : res) {
             for (int i : list) {
