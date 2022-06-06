80d1a82bb0a3f7767b131c2cd3c79b8d03b8f1aa
Gyu Park <kyesung8282@gmail.com>
2022-06-06 08:55:25 -0400

    Problem 220: Recover Binary Search Tree

--- Problems/RecoverBinarySearchTree.java
+++ Problems/RecoverBinarySearchTree.java
@@ -6,8 +6,36 @@
 package Problems;
 
 public class RecoverBinarySearchTree {
+
+    static TreeNode firstEl = null;
+    static TreeNode secondEl = null;
+    static TreeNode preEl = new TreeNode(Integer.MIN_VALUE);
+
     public static void recoverTree(TreeNode root) {
+        helper(root);
+
+        int temp = firstEl.val;
+        firstEl.val = secondEl.val;
+        secondEl.val = temp;
+    }
+
+    private static void helper(TreeNode root) {
+        if (root == null)
+            return;
         
+        helper(root.left);
+
+        // Start of "do some business", 
+        // If first element has not been found, assign it to prevElement (refer to 6 in the example above)
+        if (firstEl == null && preEl.val >= root.val)
+            firstEl = preEl;
+    
+        // If first element is found, assign the second element to the root (refer to 2 in the example above)
+        if (firstEl != null && preEl.val >= root.val)
+            secondEl = root;
+        preEl = root;
+
+        helper(root.right);
     }
 
     public static class TreeNode {
