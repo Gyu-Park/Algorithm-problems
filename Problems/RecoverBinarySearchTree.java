bb64b7f61dc4d642d3a80da73e97c1c4ff9dfd76
Gyu-Park <kyesung8282@gmail.com>
2022-06-10 21:04:06 -0400

    Problem 220: Recover Binary Search Tree

--- Problems/RecoverBinarySearchTree.java
+++ Problems/RecoverBinarySearchTree.java
@@ -6,6 +6,9 @@
 package Problems;
 
 public class RecoverBinarySearchTree {
+    // iterative solution using Morris-traversal
+    // time complexity O(n)
+    // space complexity O(1)
     public static void recoverTree(TreeNode root) {
         TreeNode pre = null;
         TreeNode first = null, second = null;
@@ -55,6 +58,46 @@ public class RecoverBinarySearchTree {
             first.val = second.val;
             second.val = t;
         }
+    }
+
+    // recursive solution using in-order traversal
+    // time complexity O(n)
+    // space complexity O(n)
+    TreeNode prev = null;
+    TreeNode first = null;
+    TreeNode second = null;
+
+    public void anotherRecoverTree(TreeNode root) {
+        // use inorder traversal to detect incorrect node
+
+        inOrder(root);
+
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null)
+            return;
+        // search left tree
+        inOrder(root.left);
+
+        // in inorder traversal of BST, prev should always have smaller value than
+        // current value
+        if (prev != null && prev.val >= root.val) {
+            // incorrect smaller node is always found as prev node
+            if (first == null)
+                first = prev;
+            // incorrect larger node is always found as curr(root) node
+            second = root;
+        }
+
+        // update prev node
+        prev = root;
+
+        // search right tree
+        inOrder(root.right);
     }
 
     public static class TreeNode {
