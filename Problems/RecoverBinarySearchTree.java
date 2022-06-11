c7e0271a184b811b5c16e65f7723c3909b31b1b2
Gyu-Park <kyesung8282@gmail.com>
2022-06-10 20:52:35 -0400

    Problem 220: Recover Binary Search Tree

--- Problems/RecoverBinarySearchTree.java
+++ Problems/RecoverBinarySearchTree.java
@@ -11,42 +11,50 @@ public class RecoverBinarySearchTree {
         TreeNode first = null, second = null;
         // Morris Traversal
         TreeNode temp = null;
-		while(root!=null){
-			if(root.left!=null){
-				// connect threading for root
-				temp = root.left;
-				while(temp.right!=null && temp.right != root)
-					temp = temp.right;
-				// the threading already exists
-				if(temp.right!=null){
-				    if(pre!=null && pre.val > root.val){
-				        if(first==null){first = pre;second = root;}
-				        else{second = root;}
-				    }
-				    pre = root;
-				    
-					temp.right = null;
-					root = root.right;
-				}else{
-					// construct the threading
-					temp.right = root;
-					root = root.left;
-				}
-			}else{
-				if(pre!=null && pre.val > root.val){
-				    if(first==null){first = pre;second = root;}
-				    else{second = root;}
-				}
-				pre = root;
-				root = root.right;
-			}
-		}
-		// swap two node values;
-		if(first!= null && second != null){
-		    int t = first.val;
-		    first.val = second.val;
-		    second.val = t;
-		}
+        while (root != null) {
+            if (root.left != null) {
+                // connect threading for root
+                temp = root.left;
+                while (temp.right != null && temp.right != root)
+                    temp = temp.right;
+                // the threading already exists
+                if (temp.right != null) {
+                    if (pre != null && pre.val > root.val) {
+                        if (first == null) {
+                            first = pre;
+                            second = root;
+                        } else {
+                            second = root;
+                        }
+                    }
+                    pre = root;
+
+                    temp.right = null;
+                    root = root.right;
+                } else {
+                    // construct the threading
+                    temp.right = root;
+                    root = root.left;
+                }
+            } else {
+                if (pre != null && pre.val > root.val) {
+                    if (first == null) {
+                        first = pre;
+                        second = root;
+                    } else {
+                        second = root;
+                    }
+                }
+                pre = root;
+                root = root.right;
+            }
+        }
+        // swap two node values;
+        if (first != null && second != null) {
+            int t = first.val;
+            first.val = second.val;
+            second.val = t;
+        }
     }
 
     public static class TreeNode {
@@ -67,13 +75,13 @@ public class RecoverBinarySearchTree {
             this.right = right;
         }
     }
-    
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
         root.left = new TreeNode(1);
         root.right = new TreeNode(4);
         root.right.left = new TreeNode(2);
         recoverTree(root);
-        
+
     }
 }
