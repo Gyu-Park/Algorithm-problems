ea544ec50bb781cc0ace4fdb943d4f2cbac847a5
Gyu Park <kyesung8282@gmail.com>
2022-06-28 10:13:00 -0400

    Problem 226: Path Sum II

--- Problems/PathSumII.java
+++ Problems/PathSumII.java
@@ -10,8 +10,71 @@ package Problems;
 import java.util.*;
 
 public class PathSumII {
+    // recursive solution
+    // time complexity O(logn) because of a copy when putting a list into res.
+    // space complexity O(logn) = the height of the tree
+    //                            but in the worst case, O(n) (imbalanced tree)
     public static List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
+        return helper(root, targetSum, res, list, sum);
+    }
 
+    private static List<List<Integer>> helper(TreeNode root, int targetSum, List<List<Integer>> res, List<Integer> list, int sum) {
+        if(root == null)
+            return res;
+        list.add(root.val);
+        sum += root.val;
+        if (root.left == null && root.right == null && sum == targetSum) {
+            res.add(new ArrayList<>(list));
+            return res;
+        }
+        if (root.left != null) {
+            res = helper(root.left, targetSum, res, list, sum);
+            list.remove(list.size() - 1);
+        }
+        if (root.right != null) {
+            res = helper(root.right, targetSum, res, list, sum);
+            list.remove(list.size() - 1);
+        }
+        return res;
+    }
+
+    // iterative solution
+    // time complexity O(log n)
+    // space complexity O(n) in worst case
+    public List<List<Integer>> anotherPathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        int sum = 0;
+        TreeNode cur = root;
+        TreeNode pre = null;
+
+        // post-order
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                path.add(cur.val);
+                sum += cur.val;
+                cur = cur.left;
+            }
+            cur = stack.peek();
+            if(cur.right != null && cur.right != pre){
+                cur = cur.right;
+                continue;
+            } 
+            if(cur.left == null && cur.right == null && sum == targetSum) 
+                res.add(new ArrayList<Integer>(path));
+  
+            pre = cur;
+            stack.pop();
+            path.remove(path.size() - 1);
+            sum -= cur.val;
+            cur = null;
+        }
+        return res;
     }
 
     public static class TreeNode {
@@ -44,7 +107,7 @@ public class PathSumII {
         TreeNode l3 = new TreeNode(7);
         TreeNode l2 = new TreeNode(11, l3, l4);
         TreeNode l1 = new TreeNode(4, l2, null);
-        TreeNode root = new TreeNode(1, l1, r1);
+        TreeNode root = new TreeNode(5, l1, r1);
         System.out.println(pathSum(root, targetSum));
     }
 }
