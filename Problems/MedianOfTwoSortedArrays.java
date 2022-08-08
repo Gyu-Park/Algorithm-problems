707b106e70c9a4e87f778c12e7958d9c692949d5
Gyu Park <kyesung8282@gmail.com>
2022-08-08 08:08:21 -0400

    Problem 12: Median of Two Sorted Arrays

--- Problems/MedianOfTwoSortedArrays.java
+++ Problems/MedianOfTwoSortedArrays.java
@@ -48,13 +48,38 @@ public class MedianOfTwoSortedArrays {
                     middlePoint - middlePoint / 2);
     }
 
+    // another binary search solution
+    // time complexity: O(log m+n)
+    // space complexity: O(1)
+    public static double anotherFindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int N1 = nums1.length;
+        int N2 = nums2.length;
+        if (N1 < N2) return findMedianSortedArrays(nums2, nums1);	// Make sure A2 is the shorter one.
+
+        int lo = 0, hi = N2 * 2;
+        while (lo <= hi) {
+            int mid2 = (lo + hi) / 2;   // Try Cut 2 
+            int mid1 = N1 + N2 - mid2;  // Calculate Cut 1 accordingly
+
+            double L1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[(mid1-1)/2];	// Get L1, R1, L2, R2 respectively
+            double L2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[(mid2-1)/2];
+            double R1 = (mid1 == N1 * 2) ? Integer.MAX_VALUE : nums1[(mid1)/2];
+            double R2 = (mid2 == N2 * 2) ? Integer.MAX_VALUE : nums2[(mid2)/2];
+
+            if (L1 > R2) lo = mid2 + 1;		// A1's lower half is too big; need to move C1 left (C2 right)
+            else if (L2 > R1) hi = mid2 - 1;	// A2's lower half too big; need to move C2 left.
+            else return (Math.max(L1,L2) + Math.min(R1, R2)) / 2;	// Otherwise, that's the right cut.
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] input1 = new int[] { 3, 4, 9, 19 };
         int[] input2 = new int[] { 5, 7, 10 };
-        int[] input3 = new int[] { 1, 2, 3, 5, 9, 11, 12, 15, 19, 31 };
-        int[] input4 = new int[] { -1, 0, 4, 18 };
+        int[] input3 = new int[] { 2, 3, 5, 9, 11, 12, 15, 19, 31, 32 };
+        int[] input4 = new int[] { -1 };
         double result = findMedianSortedArrays(input1, input2);
-        double result2 = findMedianSortedArrays(input3, input4);
+        double result2 = anotherFindMedianSortedArrays(input3, input4);
         System.out.println("result: " + result);
         System.out.println("result: " + result2);
     }
