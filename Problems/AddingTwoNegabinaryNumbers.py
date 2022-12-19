da9632a6f9477ce71adadbb1f621271209f18b83
Gyu-Park <kyesung8282@gmail.com>
2022-12-19 17:20:49 -0500

    Problem 288: Adding Two Negabinary Numbers

---
+++ Problems/AddingTwoNegabinaryNumbers.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+def addNegabinary(arr1: List[int], arr2: List[int]) -> List[int]:
+    res = []
+    carry = 0
+    while arr1 or arr2 or carry:
+        carry += (arr1 or [0]).pop() + (arr2 or [0]).pop()
+        res.append(carry & 1)
+        carry = -(carry >> 1)
+    while len(res) > 1 and res[-1] == 0:
+        res.pop()
+    return res[::-1]
+
+
+arr1 = [1, 1, 1, 1, 1]
+arr2 = [1, 0, 1]
+print(addNegabinary(arr1, arr2))
