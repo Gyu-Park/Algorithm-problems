62bd9ca34b702d4d70de1b9aa3cf3dff4d7c8ff7
Gyu-Park <kyesung8282@gmail.com>
2022-12-21 21:07:45 -0500

    Problem 293: Restore Ip Addresses

--- Problems/RestoreIPAddresses.py
+++ Problems/RestoreIPAddresses.py
@@ -5,27 +5,37 @@ from typing import List
 
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        if len(s) < 4:
-            return []
         res = []
-        st = ""
-        Solution.dfs(s, st, 0, 0, res)
+        if len(s) < 4:
+            return res
+        self.dfs(s, "", 0, res)
         return res
 
-    def dfs(self, s, st, index, count, res):
-        if count == 4 and index != len(s):
+    def dfs(self, s, st, count, res):
+        if count == 4 and 0 < len(s):
             return
-        if count == 4 and index == len(s):
+        if count == 4 and 0 == len(s):
             res.append(st[:-1])
             return
         stBuild = ""
-        for i, c in enumerate(s[index:]):
-            if i == 3:
-                break
+        for i, c in enumerate(s):
             stBuild += c
-            if int(stBuild) > 255 or (len(stBuild) > 1 and stBuild[0] == '0'):
+            if i == 3 or int(stBuild) > 255 or (len(stBuild) > 1 and stBuild[0] == '0'):
                 break
-            st += c
-            st += "."
-            Solution.helper(s, st, index + i + 1, count + 1, res)
-            st = st[:-1]
+            self.dfs(s[i + 1:], st + stBuild + ".", count + 1, res)
+
+    # another solution
+    def restoreIpAddresses2(self, s):
+        res = []
+        self.dfs2(s, 0, "", res)
+        return res
+
+    def dfs2(self, s, idx, path, res):
+        if idx == 4 and s:
+            return
+        if idx == 4 and not s:
+            res.append(path[:-1])
+            return
+        for i in range(1, len(s)+1):
+            if s[:i] == '0' or (s[0] != '0' and 0 < int(s[:i]) < 256):
+                self.dfs(s[i:], idx + 1, path + s[:i]+".", res)
