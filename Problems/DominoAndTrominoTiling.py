13667e3777b762065fcb651dfa5e1c7ea069b505
Gyu-Park <kyesung8282@gmail.com>
2022-12-23 22:09:45 -0500

    Problem 297: Domino and Tromino Tiling

--- Problems/DominoAndTrominoTiling.py
+++ Problems/DominoAndTrominoTiling.py
@@ -21,4 +21,15 @@ def numTilings(n: int) -> int:
     return f(n)
 
 
+def numTilings(N):
+    # A[i] = 2 * (N-1) board
+    # B[i] = 2 * N board.
+    A = [0] * (N + 1)
+    B = [1, 1] + [0] * (N - 1)
+    for i in range(2, N + 1):
+        A[i] = (B[i - 2] + A[i - 1]) % int(1e9 + 7)
+        B[i] = (B[i - 1] + B[i - 2] + A[i - 1] * 2) % int(1e9 + 7)
+    return B[N]
+
+
 print(numTilings(5))
