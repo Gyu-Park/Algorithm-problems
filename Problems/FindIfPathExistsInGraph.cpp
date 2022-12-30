d7a7ae5f284c54da8d2b1b7f54d155e05e9feb56
Gyu-Park <kyesung8282@gmail.com>
2022-12-29 21:07:20 -0500

    Problem 325: Find If Path Exists in Graph

--- Problems/FindIfPathExistsInGraph.cpp
+++ Problems/FindIfPathExistsInGraph.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <numeric>
 #include <queue>
 #include <vector>
 using namespace std;
@@ -31,4 +32,43 @@ class Solution {
         }
         return false;
     }
+
+    // solution using disjoint set union (dsu)
+    bool validPath2(int n, vector<vector<int>>& edges, int source, int destination) {
+        UnionFind uf(n);
+
+        for (auto& edge : edges) {
+            uf.join(edge[0], edge[1]);
+        }
+
+        return uf.find(source) == uf.find(destination);
+    }
+
+    class UnionFind {
+        vector<int> root, rank;
+
+       public:
+        UnionFind(int n) : root(n), rank(n, 1) {
+            iota(root.begin(), root.end(), 0);
+        }
+        int find(int x) {
+            if (x != root[x]) {
+                root[x] = find(root[x]);
+            }
+            return root[x];
+        }
+        void join(int x, int y) {
+            int rootX = find(x), rootY = find(y);
+
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    swap(rootX, rootY);
+                }
+                // Modify the root of the smaller group as the root of the
+                // larger group, also increment the size of the larger group.
+                root[rootX] = rootY;
+                rank[rootY] += rank[rootX];
+            }
+        }
+    };
 };
