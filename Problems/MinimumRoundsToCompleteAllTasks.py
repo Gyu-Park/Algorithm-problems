8d73529beea946b9c628feba080ecfbd406fac87
Gyu-Park <kyesung8282@gmail.com>
2023-01-04 11:03:01 -0500

    Problem 335: Minimum Rounds to Complete All Tasks

---
+++ Problems/MinimumRoundsToCompleteAllTasks.py
@@ -0,0 +1,8 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def minimumRounds(self, tasks: List[int]) -> int:
+        freq = Counter(tasks).values()
+        return -1 if 1 in freq else sum((a + 2) // 3 for a in freq)
