b12232faa7c4e232b7ed093d781b528419c60f02
Gyu-Park <kyesung8282@gmail.com>
2023-04-22 11:20:05 -0400

    Problem 358: Minimum Insertion Steps to Make a String Palindrome

--- Problems/MinimumInsertionStepsToMakeStringPalindrome.java
+++ Problems/MinimumInsertionStepsToMakeStringPalindrome.java
@@ -33,4 +33,24 @@ public class MinimumInsertionStepsToMakeStringPalindrome {
 
         return helper(s, 0, n - 1);
     }
+
+    // bottom-up dp solution
+    // find longest common sequence between s and its reverse
+    // time complexity: O(n^2)
+    // space complexity: O(n^2)
+    public int minInsertions2(String s) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s.charAt(i) == s.charAt(n - 1 - j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    continue;
+                }
+                dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+
+        return n - dp[n][n];
+    }
 }
