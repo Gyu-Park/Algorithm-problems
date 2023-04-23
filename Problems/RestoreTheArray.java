61d6c7edd2fd72ebee9d55adf635743b18711442
Gyu-Park <kyesung8282@gmail.com>
2023-04-23 11:39:52 -0400

    Problem 360: Restore The Array

--- Problems/RestoreTheArray.java
+++ Problems/RestoreTheArray.java
@@ -35,4 +35,31 @@ public class RestoreTheArray {
         memo = new int[n];
         return helper(s, k, 0);
     }
+
+    // bottom-up dp solution
+    // time complexity: O(nlogk)
+    // spcae complexity: O(n)
+    public int numberOfArrays2(String s, int k) {
+        int MOD = 1_000_000_007;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        // base case
+        dp[0] = 1;
+
+        // inductive case
+        for (int i = 1; i <= n; i++) { // n iteration
+            int index = i - 1;
+            if (s.charAt(index) == '0')
+                continue;
+            long num = 0;
+            for (int j = index; j < n; j++) { // iterate up to the number of digits of k, which log k
+                num = num * 10 + s.charAt(j) - '0';
+                if (num > k)
+                    break;
+                dp[j + 1] += dp[i - 1];
+                dp[j + 1] %= MOD;
+            }
+        }
+        return dp[n];
+    }
 }
