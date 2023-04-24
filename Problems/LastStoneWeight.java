77beea72b4fe20dfdc6d677bbaa8a53e18f44a78
Gyu-Park <kyesung8282@gmail.com>
2023-04-24 10:50:22 -0400

    Problem 362: Last Stone Weight

--- Problems/LastStoneWeight.java
+++ Problems/LastStoneWeight.java
@@ -4,6 +4,9 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class LastStoneWeight {
+    // max heap solution
+    // time complexity: O(nlogn)
+    // space complexity: O(n)
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         // PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b.compareTo(a));
@@ -21,5 +24,42 @@ public class LastStoneWeight {
         }
 
         return pq.isEmpty() == true ? 0 : pq.poll();
+    }
+
+    // counting sort solution
+    // time complexity: O(max(n, m)), m = indices of the cnt array
+    // space complexity: O(n)
+    public int lastStoneWeight2(int[] stones) {
+        int max = 0;
+        for (int s : stones) {
+            max = Math.max(max, s);
+        }
+
+        int[] cnt = new int[max + 1];
+        for (int s : stones) {
+            cnt[s]++;
+        }
+
+        int biggest = 0;
+        int curW = max;
+        while (curW > 0) {
+            if (cnt[curW] == 0) {
+                curW--;
+            } else if (biggest == 0) {
+                cnt[curW] %= 2;
+                if (cnt[curW] == 1)
+                    biggest = curW;
+                curW--;
+            } else {
+                cnt[curW]--;
+                biggest -= curW;
+                if (biggest > curW)
+                    continue;
+                cnt[biggest]++;
+                biggest = 0;
+            }
+        }
+
+        return biggest;
     }
 }
